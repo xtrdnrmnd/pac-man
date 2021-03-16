6c9ef86115af685a48f2124d886e07d9867924f7
xtrdnrmnd <polinkakub@mail.ru>
2021-03-16 01:36:26 +0100

    added some objects

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var canv = document.getElementById("myCanvas");
-var ctx = canv.getContext("2d");
+// Get canvas element and it's context
+var canv = document.getElementById('myCanvas')
+var ctx = canv.getContext('2d');
 
 // Background style: fill with black color
 ctx.fillStyle = "black";
@@ -8,23 +9,78 @@ ctx.fillRect(0, 0, canv.width, canv.height);
 // Pac-Man image logo at the top of the canvas
 var imageObj = new Image();
 imageObj.src = "Obrazky/main-page-logo.png"
-imageObj.onload = function () { ctx.drawImage(imageObj, 159, 66, 1120, 265);}
+imageObj.onload = function () { ctx.drawImage(imageObj, 159, 66, 1120, 265); }
+
+// Button position and dimensions
+var buttonW = 290;
+var buttonH = 80;
+var buttonW2 = 470;
+var buttonW3 = 220;
+
 
 // Start button text
-var x = canv.width / 2;
+var posX = canv.width / 2;
 ctx.font = '50pt Comic Sans MS';
 ctx.textAlign = 'center';
 ctx.fillStyle = '#FFFF00';
-ctx.fillText('START', x, 385);
+ctx.fillText('START', posX, 385);
+
+
+// Add event listener to canvas element
+canv.addEventListener('click', function (event) {
+    // Control that click event occurred within position of button
+    if (
+        event.x > 550 &&
+        event.x < 550 + buttonW &&
+        event.y > 320 &&
+        event.y < 320 + buttonH
+    ) {
+        // Executes if button was clicked!
+        alert('Button was clicked!');
+    }
+});
 
 // Highscore button text
 ctx.font = '50pt Comic Sans MS';
 ctx.textAlign = 'center';
 ctx.fillStyle = '#FFFF00';
-ctx.fillText('HIGHSCORES', x, 479);
+ctx.fillText('HIGHSCORES', posX, 479);
+
+// Add event listener to canvas element
+canv.addEventListener('click', function (event) {
+    // Control that click event occurred within position of button 
+    if (
+        event.x > 480 &&
+        event.x < 480 + buttonW2 &&
+        event.y > 420 &&
+        event.y < 420 + buttonH
+    ) {
+        // Executes if button was clicked!
+        alert('Button 2 was clicked!');
+    }
+});
 
 // Quit button text
 ctx.font = '50pt Comic Sans MS';
 ctx.textAlign = 'center';
 ctx.fillStyle = '#FFFF00';
-ctx.fillText('QUIT', x, 479);
+ctx.fillText('QUIT', posX, 583);
+
+// Add event listener to canvas element
+canv.addEventListener('click', function (event) {
+    // Control that click event occurred within position of button
+    if (
+        event.x > 610 &&
+        event.x < 610 + buttonW3 &&
+        event.y > 520 &&
+        event.y < 520 + buttonH
+    ) {
+        // Executes if button was clicked!
+        alert('Button 3 was clicked!');
+    }
+});
+
+// Volume image
+var imageVolume = new Image();
+imageVolume.src = "Obrazky/volume-off.png"
+imageVolume.onload = function () { ctx.drawImage(imageVolume, 642, 618, 156, 149,); }
